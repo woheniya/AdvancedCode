749db3bb97fa3e5d44c88e1cf1f068a0c8b607b4
cheng <736113252@qq.com>
2020-08-13 00:13:32 +0800

    0813第一次多线程提交

---
+++ ThreadLearning/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ThreadLearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.btnThread = new System.Windows.Forms.Button();
+            this.btnThreadpool = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(145, 75);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(145, 136);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnAsync.TabIndex = 1;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // btnThread
+            // 
+            this.btnThread.Location = new System.Drawing.Point(145, 197);
+            this.btnThread.Name = "btnThread";
+            this.btnThread.Size = new System.Drawing.Size(75, 23);
+            this.btnThread.TabIndex = 2;
+            this.btnThread.Text = "Thread";
+            this.btnThread.UseVisualStyleBackColor = true;
+            // 
+            // btnThreadpool
+            // 
+            this.btnThreadpool.Location = new System.Drawing.Point(145, 258);
+            this.btnThreadpool.Name = "btnThreadpool";
+            this.btnThreadpool.Size = new System.Drawing.Size(75, 23);
+            this.btnThreadpool.TabIndex = 3;
+            this.btnThreadpool.Text = "ThreadPool";
+            this.btnThreadpool.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThreadpool);
+            this.Controls.Add(this.btnThread);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnSync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnAsync;
+        private System.Windows.Forms.Button btnThread;
+        private System.Windows.Forms.Button btnThreadpool;
+    }
+}
+
---
+++ ThreadLearning/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadLearning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Private Method
+        /// <summary>
+        /// 一个比较耗时耗资源的私有方法
+        /// </summary>
+        /// <param name="name"></param>
+        private void DoSomethingLong(string name)
+        {
+            Console.WriteLine($"****************DoSomethingLong Start  {name}  {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}***************");
+            long lResult = 0;
+            //for (int i = 0; i < 1000_000_000; i++)
+            //{
+            //    lResult += i;
+            //}
+            Thread.Sleep(2000); //线程是闲置的；
+            Console.WriteLine($"****************DoSomethingLong   End  {name}  {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} {lResult}***************");
+        }
+        #endregion
+        /// <summary>
+        /// 同步
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            //基础线程Thread.CurrentThread.ManagedThreadId 对标于每一个线程的一个ID;
+            {
+                //Console.WriteLine($"****************btnSync_Click Start {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}***************");
+                //int l = 3;
+                //int m = 4;
+                //int n = l + m;
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    string name = string.Format($"btnSync_Click_{i}");
+                //    this.DoSomethingLong(name);
+                //}
+                //Console.WriteLine($"****************btnSync_Click   End {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}***************");
+            }
+            //控制线程的调用顺序.无返回值
+            {
+                //控制线程的调用顺序
+                //ThreadStart threadStart = () =>
+                //{
+                //    Console.WriteLine("你好啊，兄嘚");
+                //};
+                //Action action = () =>
+                //{
+                //    Console.WriteLine("你好啊，兄嘚姐妹");
+                //};
+                //action += () =>
+                //{
+                //    Console.WriteLine("你好啊");
+                //};
+                //action += () =>
+                //{
+                //    Console.WriteLine("Hello");
+                //};
+                ////包一层大法，不卡界面
+                ////Thread thread = new Thread(threadStart);
+                //ThreadTest(threadStart, action);
+            }
+            //控制线程的调用顺序.有返回值
+            {
+                Func<int> func = () =>
+                {
+                    Thread.Sleep(5000);
+                    return DateTime.Now.Year;
+                };
+
+                Func<int> yfun = ThreadHasReturn(func);
+                Console.WriteLine(yfun.Invoke());
+
+                //Console.WriteLine(ThreadHasReturn(func));
+            }
+
+        }
+        /// <summary>
+        /// 异步
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAsync_Click(object sender, EventArgs e)
+        {
+            //异步初相识
+            {
+                Console.WriteLine($"****************btnAsync_Click Start {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}***************");
+
+                Action<string> action = this.DoSomethingLong;
+                action.Invoke("btnAsync_Click");
+                //action("btnAsync_Click");
+
+                action.BeginInvoke("btnAsync_Click", null, null); //会分配一个新的线程去执行；20200803Core还不支持；
+
+                for (int i = 0; i < 5; i++)
+                {
+                    string name = string.Format($"btnAsync_Click_{i}");
+                    action.BeginInvoke(name, null, null);
+                }
+                Console.WriteLine($"****************btnAsync_Click End   {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}***************");
+            }
+            
+            //回调函数
+
+        }
+        //无返回值
+        private void ThreadNoReturn(ThreadStart threadStart,Action action)
+        {
+            ThreadStart thread = () =>
+            {
+                threadStart.Invoke();
+                action.Invoke();
+            };
+
+            Thread thread1 = new Thread(thread);
+            thread1.Start();
+        }
+        //有返回值
+        private Func<T> ThreadHasReturn<T>(Func<T> func)
+        {
+
+            #region 不等待无法得到返回结果
+            //T t = default(T);
+            //ThreadStart threadStart = () =>
+            //{
+            //    t = func.Invoke();
+            //};
+            //Thread thread = new Thread(threadStart);
+            //thread.Start();
+            //return t;
+            #endregion
+            T t = default(T);
+            ThreadStart threadStart = () =>
+            {
+                t = func.Invoke();
+            };
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            return new Func<T>(() =>
+            {
+                thread.Join();
+                return t;
+            });
+
+            //疑问一
+            //thread.Join();
+            //return t;
+
+
+        }
+    }
+}
